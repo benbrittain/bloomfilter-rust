8e1b3d7e2f1885c19fbdd9737864ee5cd95a8790
Ben Brittain <ben@brittain.org>
2012-06-08 17:12:30 -0400

    allowing external hashing functions

--- bloomfilter.rs
+++ bloomfilter.rs
@@ -6,42 +6,35 @@ import bitv = std::bitv;
 import sha1 = std::sha1;
 
 #[doc = "Bloom Filter Type"]
-type bloomfilter = @{storage: bitv::bitv, capacity: uint, mut count: uint};
+type bloomfilter = @{storage: bitv::bitv, capacity: uint, mut count: uint, hash_funcs: [(fn@(str) -> [u8])]};
 
-fn bloomfilter(capacity: uint) -> bloomfilter {
-    io::println("Capacity: " + uint::to_str(capacity, 10u));
-    @{storage: bitv::bitv(capacity,false), capacity: capacity, mut count: 0u}
+fn bloomfilter(capacity: uint, hash_funcs: [(fn@(str) -> [u8])]) -> bloomfilter {
+    @{storage: bitv::bitv(capacity,false), capacity: capacity, mut count: 0u, hash_funcs: copy hash_funcs}
 }
 
 fn add(bloomfilter: bloomfilter, elem: str){
- //   log(error,bloomfilter.capacity);
-    let sha1 = sha1::sha1();
-    sha1.input_str(elem);
-    let hashstr = sha1.result();
-    sha1.reset();
-    let mut i = 0u;
-    while i < 20u {
-        let loc = (hashstr[i] as uint) % (bloomfilter.capacity - 1u);
-        i += 1u;
-        bitv::set(bloomfilter.storage, loc, true);
-    } 
+    for vec::each(bloomfilter.hash_funcs) {|func|
+        let hashstr:[u8] = func(elem); 
+            for vec::each(hashstr) {|elm|
+                let loc = (elm as uint) % (bloomfilter.capacity - 1u);
+                bitv::set(bloomfilter.storage, loc, true);
+            }
+    }
     bloomfilter.count += 1u;
     assert bloomfilter.count <= bloomfilter.capacity;
 }
 
 fn contains(bloomfilter: bloomfilter, elem: str) -> bool {
-    let sha1 = sha1::sha1();
-    sha1.input_str(elem);
-    let hashstr = sha1.result();
-    sha1.reset();
-    let mut i = 0u;
-    while i < 20u {
-        let loc = (hashstr[i] as uint) % (bloomfilter.capacity - 1u);
-        let val = bitv::get(bloomfilter.storage, loc);
-        if val == false {
-            ret false;
-        } 
-        i += 1u;
+    for vec::each(bloomfilter.hash_funcs) {|func|
+        let hashstr:[u8] = func(elem); 
+            for vec::each(hashstr) {|elm|
+                let loc = (elm as uint) % (bloomfilter.capacity - 1u);
+                let val = bitv::get(bloomfilter.storage, loc);
+                if val == false {
+                    ret false;
+                } 
+                bitv::set(bloomfilter.storage, loc, true);
+            }
     }
     ret true;
 } 
